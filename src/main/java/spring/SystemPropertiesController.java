c06bc74fea16d759efeab009c9f1588ddc40e62e
jaco0646 <1025949+jaco0646@users.noreply.github.com>
2021-01-22 10:05:40 -0600

    SystemPropertiesController

---
+++ src/main/java/spring/SystemPropertiesController.java
@@ -0,0 +1,33 @@
+package spring;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+import static java.util.Collections.emptyMap;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.*;
+
+@RestController
+public class SystemPropertiesController {
+
+    @PostMapping("/env")
+    public Map<?,?> setParams(@RequestParam Map<String, String> params) {
+        params.forEach(System::setProperty);
+        return getParams(emptyMap());
+    }
+
+    @DeleteMapping("/env")
+    public Map<?,?> deleteParams(@RequestParam Map<String, String> params) {
+        params.keySet().forEach(System::clearProperty);
+        return getParams(emptyMap());
+    }
+
+    @GetMapping("/env")
+    public Map<?,?> getParams(@RequestParam Map<String, String> params) {
+        return params.isEmpty()
+                ? System.getProperties()
+                : params.keySet().stream().collect(toMap(identity(), key -> System.getProperty(key, "")));
+    }
+
+}
