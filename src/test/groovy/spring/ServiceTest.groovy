ace89790800ba84b3f3b9b91d75ba6b0567fde87
jaco0646 <1025949+jaco0646@users.noreply.github.com>
2021-02-05 22:13:48 -0600

    SpringBootTest to assert caching

---
+++ src/test/groovy/spring/ServiceTest.groovy
@@ -0,0 +1,29 @@
+package spring
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cache.CacheManager
+import org.springframework.cache.interceptor.SimpleKey
+import spock.lang.Specification
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE
+
+@SpringBootTest(webEnvironment = NONE)
+class ServiceTest extends Specification {
+
+    @Autowired
+    CacheManager cacheManager
+
+    @Autowired
+    Service service
+
+    def cachingTest() {
+        given:
+            def cache = cacheManager.getCache("myCacheName")
+        expect:
+            cache.get(SimpleKey.EMPTY, String) == null
+            service.foo() == 'foo'
+            cache.get(SimpleKey.EMPTY, String) == 'foo'
+    }
+
+}
