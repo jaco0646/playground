1eb919aeaaf5d602df83b43bcf14c7302acd3f8c
jaco0646 <1025949+jaco0646@users.noreply.github.com>
2021-07-27 16:19:36 -0500

    Document and filter system properties.

--- src/main/java/spring/SystemPropertiesController.java
+++ src/main/java/spring/SystemPropertiesController.java
@@ -9,6 +9,16 @@ import static java.util.Collections.emptyMap;
 import static java.util.function.Function.identity;
 import static java.util.stream.Collectors.*;
 
+/**
+ * This controller in combination with a {@code /restart} endpoint can be used to update Spring application properties,
+ * without redeploying the application, by setting a json value for the {@code SPRING_APPLICATION_JSON} property.
+ * <p>
+ * For example, an HTTP encoded value such as {@code %7B%22message%22%3A%22runtime-json-message%22%7D} sets the Spring
+ * {@code message} property to a value of {@code runtime-json-message}.
+ *
+ * @see <a href="https://docs.spring.io/spring-boot/docs/current/reference/html/features.html#features.external-config"/>
+ *               https://docs.spring.io/spring-boot/docs/current/reference/html/features.html#features.external-config</a>
+ */
 @RestController
 public class SystemPropertiesController {
 
@@ -27,8 +37,17 @@ public class SystemPropertiesController {
     @GetMapping("/env")
     public Map<?,?> getParams(@RequestParam Map<String, String> params) {
         return params.isEmpty()
-                ? new TreeMap<>(System.getProperties())
+                ? getSystemProperties()
                 : params.keySet().stream().collect(toMap(identity(), key -> System.getProperty(key, "")));
+    }
+
+    private Map<?,?> getSystemProperties() {
+        Map<?, ?> sysProps = new TreeMap<>(System.getProperties());
+        // Exclude keys with very long values to improve readability.
+        // System properties are available in read-only json format via Spring Boot Actuator anyway.
+        sysProps.remove("java.class.path");
+        sysProps.remove("java.library.path");
+        return sysProps;
     }
 
 }
