779a623bbcd49059e6d094153869e240954ba288
jaco0646 <1025949+jaco0646@users.noreply.github.com>
2021-09-23 14:14:31 -0500

    Example @Embedded @Entity. Enable H2 Web Console.

---
+++ src/main/java/spring/jpa/Triple.java
@@ -0,0 +1,30 @@
+package spring.jpa;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+import javax.persistence.*;
+
+import static lombok.AccessLevel.PACKAGE;
+
+@ImmutableEntity(tableName = "triple")
+@Value
+@NoArgsConstructor(access = PACKAGE, force = true)
+@AllArgsConstructor(access = PACKAGE)
+public class Triple {
+    /**
+     * An @Entity can only be embedded in another @Entity if they share an @Id.
+     * Generally, the embedded class would not be another @Entity. It would be
+     * annotated with @Embeddable instead.
+     */
+    @Id
+    @Column(name = "k")
+    String key;
+
+    @Embedded
+    KeyValue kv;
+
+    @Column
+    String third;
+}
