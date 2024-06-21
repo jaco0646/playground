402902ad9a3025e91bf043ecda88cb2ed7b8bcd1
jaco0646 <1025949+jaco0646@users.noreply.github.com>
2024-06-21 16:50:18 -0500

    Compare collections of different subtypes.

--- src/test/groovy/generics/SubCompareSpec.groovy
+++ src/test/groovy/generics/SubCompareSpec.groovy
@@ -2,32 +2,40 @@ package generics
 
 import spock.lang.Specification
 
+import java.time.LocalDate
+
 import static generics.SubCompare.*
 
 class SubCompareSpec extends Specification {
 
     def 'Compare lists of different subtypes'() {
         given:
-            MyInterface a = new Impl1(42L, "foo 1", 1)
-            MyInterface b = new Impl1(43L, "foo 2", 2)
-            MyInterface c = new Impl2(null, "foo 1", 1, List.of("c"))
-            MyInterface d = new Impl2(null, "foo 2", 2, List.of("d"))
+            LocalDate today = LocalDate.now()
+            LocalDate yesterday = today.minusDays(1)
+            LocalDate tomorrow = today.plusDays(1)
+            MyInterface a = new Impl1(42L, today, tomorrow, 1)
+            MyInterface b = new Impl1(43L, yesterday, today, 2)
+            MyInterface c = new Impl2(null, today, tomorrow, 1, List.of("c"))
+            MyInterface d = new Impl2(null, yesterday, today, 1, List.of("d"))
+            MyInterface e = new Impl2(null, tomorrow, today, 1, List.of("e"))
         expect:
-            equals([], [], MyInterface::foo)
-            equals([a], [c], MyInterface::foo, MyInterface::bar)
-            !equals([a], [c], MyInterface::foo, MyInterface::bar, MyInterface::id)
-            !equals([a], [b], MyInterface::foo, MyInterface::bar)
-            equals([a, b], [c, d], MyInterface::foo, MyInterface::bar)
-            !equals([a, b], [c, c], MyInterface::foo)
+            equals([], [], MyInterface::start)
+            equals([a], [c], MyInterface::start, MyInterface::end, MyInterface::weight)
+            !equals([a], [c], MyInterface::start, MyInterface::end, MyInterface::id)
+            !equals([a], [b], MyInterface::start, MyInterface::end)
+            !equals([a], [e], MyInterface::start, MyInterface::end)
+            equals([a, b], [c, d], MyInterface::start, MyInterface::end)
+            !equals([a, b], [d, e], MyInterface::end)
     }
 
     interface MyInterface {
         Long id();
-        String foo();
-        int bar();
+        LocalDate start();
+        LocalDate end();
+        Integer weight();
     }
 
-    static record Impl1(Long id, String foo, int bar) implements MyInterface { }
-    static record Impl2(Long id, String foo, int bar, List<String> baz) implements MyInterface { }
+    static record Impl1(Long id, LocalDate start, LocalDate end, Integer weight) implements MyInterface { }
+    static record Impl2(Long id, LocalDate start, LocalDate end, Integer weight, List<String> baz) implements MyInterface { }
 
 }
